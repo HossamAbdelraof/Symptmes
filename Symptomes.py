273b98bc588d1d681920d11b2f4e97e96d1bb0a8
Hossam Abdelraof <81495150+HossamAbdelraof@users.noreply.github.com>
2025-05-16 00:51:10 +0300

    Update Symptomes.py

--- Symptomes.py
+++ Symptomes.py
@@ -69,3 +69,9 @@ def predict(request: SymptomRequest):
         })
 
     return {"predictions": results}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
